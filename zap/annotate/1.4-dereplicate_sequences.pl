b32fc5fb7cf26f55c284399dd6c12c391c013938
zizhang sheng <sheng@c2b2bhmd1.c2b2.columbia.edu>
2015-04-28 09:56:24 -0400

    modified:   zap/annotate/1.4-dereplicate_sequences.pl

--- zap/annotate/1.4-dereplicate_sequences.pl
+++ zap/annotate/1.4-dereplicate_sequences.pl
@@ -8,10 +8,9 @@ Usage:
 This script performs two steps of clustering to remove sequences potentially containing sequencing errors. The first step finds duplicates, remove reads with coverage lower then cutoff which may contain sequencing errors, calculate read coverage for each cluster. The second step further cluster the filtered sequences using lower sequence identity cutoff. At the meantime, the second clustering will use reads with high coverage as centroids by assuming biological reads are coming from cDNA with many identical copies while reads containing sequencing errors has very low coverage. please install usearch v7 or higher verion. 
 
 options:
- \t-id1\t percent sequence identity used for the first step of clustering, default:1.00
- \t-id2\t percent sequence identity used for the second step of clustering, default:0.99
- \t-min1\tminimun coverage of a read to be kept in the first step of clustering, default:2
- \t-min2\tminimun coverage of a read to be kept in the seconde step of clustering, default:3
+ \t-id\t percent sequence identity used for the second step of clustering, default:0.99
+ \t-min1\tminimun sequencing coverage of a read to be kept in the first step of clustering, default:2
+ \t-min2\tminimun sequencing coverage of a read to be kept in the seconde step of clustering, default:3
  \t-f\tsequence file in fasta format
  \t-t\tnumber of threads to run the script. Default:1
 Example:
@@ -35,8 +34,7 @@ if(!$para{'-f'}){
 	else{die "no input seq file\n";}
 }
 if(!$para{'-t'}){$para{'-t'}=1;}
-if(!$para{'-id2'}){$para{'-id2'}=0.99;}
-if(!$para{'-id1'}){$para{'-id1'}=1;}
+if(!$para{'-id'}){$para{'-id'}=0.99;}
 #########do calculation##########
 
 my $output=&usearch($para{'-f'},$para{'-p'},$para{'-id'});
@@ -50,68 +48,18 @@ sub usearch{#do the two steps of clustering
     my %derep=();
 	  	my %final_good=();
 	  	system("$para{'-pu'} -derep_fulllength $file -threads $para{'-t'} -fastaout $file_out\_unique.fa -sizeout -uc $file_out.cluster ");#first step on higher identity
-	  	if(-z "$file_out\_unique.fa"){die "No duplicate sequence found in your input sample. Try with lower id1\n";}
+	  	if(-z "$file_out\_unique.fa"){die "No duplicate sequence found in your input sample.\n";}
 	  	system("$para{'-pu'} -sortbysize $file_out\_unique.fa -minsize $para{'-min1'} -fastaout $file_out.nonredundant.fa");
-	  	if(-z "$file_out.nonredundant.fa"){die "No duplicate sequence found in your input sample.Try with lower id1\n";}
-	  	system("$para{'-pu'} -cluster_smallmem $file_out.nonredundant.fa -sortedby size -id $para{'-id2'} -sizein -sizeout -uc $file_out.cluster -centroids $file_out\_unique.fa ");#second step on higher identity
+	  	if(-z "$file_out.nonredundant.fa"){die "No duplicate sequence found in your input sample.\n";}
+	  	system("$para{'-pu'} -cluster_fast $file_out.nonredundant.fa -sort size -id $para{'-id'} -sizein -sizeout -uc $file_out.cluster -centroids $file_out\_unique.fa ");#second step on higher identity
 	  	if(-z "$file_out\_unique.fa"){die "No cluster found for your sequences.\n";}
-	  	%final_good=&derep("$file_out.cluster",$para{'-min2'},\%derep);#remove low coverage clusters
-	  	system("rm $file_out.nonredundant.fa");
+	  	system("$para{'-pu'} -sortbysize $file_out\_unique.fa -minsize $para{'-min2'} -fastaout $file_out.nonredundant.fa");	  	
+	  	system("mv $file_out.nonredundant.fa $file_out\_unique.fa");
   	  unlink "usearchlog.txt";
 	  return "$file_out\_unique.fa";
 }
 
 
-#####################
-sub derep{#This is used to remove low abundant clusters after the second step of clustering
-    my ($file,$cutoff,$ids)=@_;
-    open HH,"$file" or die "Usearch clustering file $file not found\n";
-    my %ids=();
-    if($ids){
-    	while(<HH>){
-    		my @line=split/[ \t]+/,$_;
-    		$line[8]=~s/\;.+//;
-    		$line[9]=~s/\;.+//;
-        if($_=~/^C/){ 
-        	 if($line[2]<$cutoff){
-        	 	foreach(@{$ids{$line[8]}}){
-        	   delete $ids->{$_};	
-        	 }
-        	}
-        }
-        elsif($_=~/^S/){
-	    	  push @{$ids{$line[8]}},$line[8];
-	      }
-	      else{
-	    	  push @{$ids{$line[9]}},$line[8];
-	      }
-      }
-      close HH;
-      return %{$ids};
-    }
-    else{
-    	while(<HH>){
-    		my @line=split/[ \t]+/,$_;
-    		$line[8]=~s/\;.+//;
-    		$line[9]=~s/\;.+//;
-      if($_=~/^C/){       	
-	     	 if($line[2]<$cutoff){
-	     	     delete $ids{$line[8]};	     
-	     	}
-	    }	
-	    elsif($_=~/^S/){
-	    	push @{$ids{$line[8]}},$line[8];
-	    }
-	    else{
-	    	push @{$ids{$line[9]}},$line[8];
-	    }
-	   	
-     }
-     close HH;
-     return %ids;
-    }	
-	
-}
 #####################
 sub write_raw{
 	 my ($good,$output)=@_;
