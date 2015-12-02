a5c5f74d575dc5038c964837a8fcef1c080c427a
Chaim A Schramm <cs3037@c2b2.columbia.edu>
2015-12-02 11:20:48 -0500

    Added finalized plotting scripts (4.1 and 4.2)

--- sonar/plotting/4.1-setup_plots.pl
+++ sonar/plotting/4.1-setup_plots.pl
@@ -7,51 +7,76 @@
  This script parses SONAR output tables and generates small flat files for
        easy plotting with 4.2-generate_plots.R.
 
- Usage: 4.1-setup_plots.pl
+ Usage: 4.1-setup_plots.pl --statistic ntraw --subset orf
+                           [ --compare ../other_project1 (...) 
+                             --output output/plots/plot_of_ntraw_for_orf.png
+                             --plotOpt "title=ProjectName" --plotOpt "h=3" (...)
+                             --list readsToInclude.txt
 
  Invoke with -h or --help to print this documentation.
 
  Parameters:
-     --plot    => Statistic to be plotted. Options [with description]:
-                      * ntraw     [raw read length in nucleotides]
-                      * aaraw     [raw read length in amino acids]
-                      * nttrim    [trimmed read length in nucleotides]
-                      * aatrim    [trimmed read length in amino acids]
-                      * ntcdr3    [cdr3 length in nucleotides]
-                      * aacdr3    [cdr3 length in amino acids]
-                      * v         [V gene usage]
-                      * j         [J gene usage]
-                      * d         [D gene usage, ignores unassigned reads]
-                      * c         [subtype usage, ignores unassigned reads]
-                      * div       [germline divergence (nt %) from muscle]
-                      * blastdiv  [estimated germline divergence from blast]
-                      * charge    [cdr3 charge, not including possible TYS]
-                      * features  [presence of indels and stop codons]
-                      * status    [V/J assignment success, in-frame, ORF, unique]
-                      * size      [number of NGS reads per representative]
+     --statistic => Statistic to be plotted. Options [with description]:
+                        * ntraw     [raw read length in nucleotides]
+                        * aaraw     [raw read length in amino acids]
+                        * nttrim    [trimmed read length in nucleotides]
+                        * aatrim    [trimmed read length in amino acids]
+                        * ntcdr3    [cdr3 length in nucleotides]
+                        * aacdr3    [cdr3 length in amino acids]
+                        * v         [V gene usage]
+                        * j         [J gene usage]
+                        * d         [D gene usage, ignores unassigned reads]
+                        * c         [subtype usage, ignores unassigned reads]
+                        * div       [germline divergence (nt %) from muscle]
+                        * blastdiv  [estimated germline divergence from blast]
+                        * charge    [cdr3 charge, not including possible TYS]
+                        * features  [presence of indels and stop codons]
+                        * status    [V/J assignment success, in-frame, ORF, unique]
+                        * size      [number of NGS reads per representative]
 
-     --subset  => Which sequence set to plot for. Not relevant for the "status"
-                      or "size" plots, which can only be done for 'all' and 
-                      'unique', repsectively. Otherwise can be specified multiple
-                      time to see how distributions shift.
-                  Options are:
-                      * all
-                      * orf (default)    
-                      * unique
-                      * manual (use --list to specify)
+     --subset    => Which sequence set to plot for. Default is 'orf' except for
+                        "status" "features" or "size" plots, which default to 'all' 
+                        'all' and 'unique', respectively, and can only be overridden
+                        with the manual option. Otherwise can be specified multiple
+                        multiple time to see how distributions shift.
+                    Options are:
+                        * all
+                        * orf (default)    
+                        * unique
+                        * manual (use --list to specify)
 
-     --compare => Add another dataset to the same plot. Can specify another plot
-                     type, eg nttrim to go with ntraw or div and blastdiv
-                     (WARNING: no sanity checks, it'll put nttrim and div on the
-                     same plot if you tell it to!); another subtype; or another 
-                     sequencing run (by path to project folder). Can be specified
-                     multiple times, but user discretion is key.
+     --compare   => Add another dataset to the same plot. Can specify another plot
+                       type, eg nttrim to go with ntraw or div and blastdiv
+                       (WARNING: no sanity checks, it'll put nttrim and div on the
+                       same plot if you tell it to!); another subtype; or another 
+                       sequencing run (by path to project folder). Can be specified
+                       multiple times, but user discretion is key.
 
-     --output  => File name for output plot. Filetype (png, pdf, etc) specified
-                     automatically by extension. Will be saved in the output/plots
-                     directory. Default file name is plot_of_PLOT_for_SUBSET.png.
+     --output    => File name for output plot. Filetype (png, pdf, etc) specified
+                       automatically by extension. Default file name is 
+                       plot_of_PLOT_for_SUBSET.png in the output/plots directory. 
 
-     --title   => Display title for plot. Default is just the current project name.
+     --plotOpt   => Formatting options to pass to the actual plotting script in the 
+                       form of "option=value". Can be specified multiple times.
+                    Possible values (with defaults in parentheses) are:
+                       * title (project name)
+                       * bars [stack or dodge] (dodge except for features and status)
+                       * percent (T)
+                       * xlab (number OR category)
+                       * ylab (percent OR number)
+                       * xlim (chosen by system)
+                       * ylim (chosen by system)
+                       * logx (F except for size plot)
+                       * logy (F except for size plot)
+                       * mids [ie plot histogram bars at midpoint of bin]
+                              ( T for nt raw/trim and aa raw/trim;
+                                F for nt/aa CDR3, div/blastdiv, and charge )
+                       * magnify (1)
+                       * showlegend (T)
+                       * legendpos ("right")
+                       * h [figure height] (chosen by system)
+                       * w [figure width] (chosen by system)
+                       * dpi (150)
 
      --list    => If --subset manual is specificed, this opition should point to a
                      text file with one sequence ID per line and statistics will be
@@ -80,23 +105,29 @@ use Bio::SeqIO;
 
 if ($#ARGV < 0) { pod2usage(1); }
 
-my @compare  = ( );
-my ($type, $subset, $outFile, $title, $listFile, $help) = ( "ntraw", "orf", "", "", "", 0 );
-GetOptions("plot=s"    => \$type,
-	   "subset=s"  => \$subset,
-	   "compare=s" => \@compare,
-	   "output=s"  => \$outFile,
-	   "title=s"   => \$title,
-	   "list=s"    => \$listFile,
-           "help!"     => \$help
+my @compare     = ( );
+my @plotOptions = ( );
+my ($type, $subset, $outFile, $listFile, $help) = ( "ntraw", "orf", "", "", 0 );
+GetOptions("statistic=s" => \$type,
+	   "subset=s"    => \$subset,
+	   "compare=s"   => \@compare,
+	   "output=s"    => \$outFile,
+	   "plotOpt=s"   => \@plotOptions,
+	   "list=s"      => \$listFile,
+           "help!"       => \$help
     );
 
 if ($help) { pod2usage(1); }
 
 
 #set up some defaults
+# put them at front of options array so they can be overriden if user specified alternates
 $outFile = "output/plots/plot_of_$type\_for_$subset.png" if $outFile eq "";
-$title   =  basename( getcwd ) if $title eq "";
+unshift @plotOptions, "title=".basename( getcwd );
+unshift @plotOptions, ("logx=T","logy=T") if $type =~ /size/;
+unshift @plotOptions, "mids=T" if $type =~ /(raw|trim)/;
+unshift @plotOptions, "bars=stack" if $type =~ /(features|status)/;
+
 
 #array to store output
 my @lines = ();
@@ -115,8 +146,9 @@ for my $over (@compare) {
 open OUT, ">work/internal/dataForPlotting.txt" or die "Can't write to work/internal/dataForPlotting.txt: $!\n\n";
 print OUT join("\n",@lines)."\n";
 close OUT;
+
 #call 4.2 with appropriate options
-system( "4.2-plot_histograms.R work/internal/dataForPlotting.txt $outFile $title");
+system( "4.2-plot_histograms.R work/internal/dataForPlotting.txt $outFile " . join(" ",@plotOptions) );
 
 
 sub chooseSubset {
@@ -129,7 +161,7 @@ sub chooseSubset {
 	case (/^a/i) { $keepVal = 0;  }
 	case (/^u/i) { $keepVal = 2;  }
 	case (/^m/i) {
-	    $keepVal = 999;
+	    $keepVal = 3;
 	    if (-e($listFile)) {
 		open LIST, $listFile or die "Can't read from $listFile: $!\n\n";
 		while (<LIST>) {
@@ -143,14 +175,14 @@ sub chooseSubset {
 	}
     }
 
-    &chooseStatistic($stat, $linesRef, $project, $keepVal, \%lookUp )
+    &chooseStatistic($stat, $linesRef, $project, $keepVal, \%lookUp, $subset )
 
 }
 
 
 sub chooseStatistic {
 
-    my ($stat, $linesRef, $project, $keepVal, $listRef) = @_;
+    my ($stat, $linesRef, $project, $keepVal, $listRef, $subset) = @_;
  
     my $colNum = 3;
     my $type = "hist"; 
@@ -163,18 +195,28 @@ sub chooseStatistic {
 	case /aaraw/    { $divide = 3; }
 	case /aatrim/   { $colNum = 4; $divide = 3; }
 	case /aacdr3/   { $colNum = 14; }
-	case /^v$/i     { $colNum = 5; $type = "count"; $divide = "\*"; }
-	case /^j$/i     { $colNum = 7; $type = "count"; $divide = "\*"; }
-	case /^d$/i     { $colNum = 6; $type = "count"; $divide = "\*"; }
-	case /^c$/i     { $colNum = 8; $type = "count"; $divide = "\*"; }
-	case /div/      { $colNum = 19; }
+	case /^v$/i     { $colNum = 5; $type = "count"; $divide = '\*'; }
+	case /^j$/i     { $colNum = 7; $type = "count"; $divide = '\*'; }
+	case /^d$/i     { $colNum = 6; $type = "count"; $divide = '\*'; }
+	case /^c$/i     { $colNum = 8; $type = "count"; $divide = '\*'; }
+	case /^div/     { $colNum = 18; }
 	case /blast/    { $colNum = 12; }
 	case /charge/   { $colNum = 15; $divide = "charge"; }
-	case /features/ { $colNum = 9; $type = "count"; }
-	case /status/   { print "Using subset=all to plot read statuses...\n\n";
-			  $keepVal = 0; $colNum = 11; $type = "count"; }
-	case /size/     { print "Using subset=unique to plot read sizes...\n\n";
-			  $keepVal = 2; $colNum = 18; }
+	case /features/ { if ($keepVal != 3) {
+	                           print "Using subset=all to plot features...\n\n";
+				   $keepVal = 0;
+			  }
+			  $colNum = 9; $type = "count"; }
+	case /status/   { if ($keepVal != 3) {
+	                           print "Using subset=all to plot read statuses...\n\n";
+				   $keepVal = 0;
+			  }
+			  $colNum = 11; $type = "count"; }
+	case /size/     { if ($keepVal != 3) {
+	                           print "Using subset=unique to plot read sizes...\n\n";
+				   $keepVal = 2; 
+			  }
+			  $colNum = 17; }
 	else {
 	    print "Sorry, I don't recognize the plot option $stat. Acceptable values are:\n\t";
 	    print join("\n\t", ("ntraw",'aaraw','nttrim','aatrim','ntcdr3','aacdr3','v','j','d','c','div','blastdiv','charge','features','status','size'));
@@ -182,15 +224,15 @@ sub chooseStatistic {
 	}
     }
 
-    push @$linesRef, @{&plot($project, $keepVal, $listRef, $colNum, $type, $divide)}; 
-    #I think the fact that I am passing by reference from the main program means I don't need to return the modified array
+    push @$linesRef, @{&plot($project, $keepVal, $listRef, $colNum, $type, $divide, $subset, $stat)}; 
+    #I am passing by reference from the main program so I don't need to return the modified array
 
 }
 
 
 sub plot {
 
-    my ($project, $subset, $listRef, $column, $type, $divide) = @_;
+    my ($project, $subset, $listRef, $column, $type, $divide, $subsetType, $statType) = @_;
 
     #for results
     my @values;
@@ -198,38 +240,61 @@ sub plot {
 
     my $prj_name = basename($project);
     open DATA, "$project/output/tables/$prj_name\_all_seq_stats.txt" or die "Can't read $project/output/tables/$prj_name\_all_seq_stats.txt: $!\n\n";
+    my $header = <DATA>;
     while (<DATA>) {
 	chomp;
 	my @arr = split;
 	
+	#error checking
+	if ($subset == 2 && $#arr < 17) {
+	    die "Cannot parse unique sequences from $prj_name; please run 1.4-dereplicate_seqeuences.pl and try again\n\n";
+	}
+	if ($column==18 && $#arr<18) {
+	    if ($#arr == 16) {
+		#assume this means that 2.1 was run without running 1.4 first and just roll with it
+		$column = 16;
+	    } else {
+		die "Cannot parse cluster sizes from $prj_name; please run 2.1-calulate_id-div.pl and try again\n\n";
+	    }
+	}
+
+	#don't count things that shouldn't be counted
+	next if $arr[$column] eq "NA";
+
 	#check if this sequence matches the subset
-	my $value = 0; $value++ if $arr[12] =~ /T/; $value++ if $arr[17] =~ /T/;
+	my $value = 0; $value++ if $arr[11] eq "good"; $value++ if $#arr>=16 && $arr[16] =~ /T/;
 	next unless ($value >= $subset || -defined($listRef->{$arr[0]}));
 	
 	
 	#special cases
 	#features
-	if ($arr[9] =~ /T/) {
-	    if ($arr[10] =~ /T/) {
-		$arr[9] = "both";
+	if ($column == 9) {
+	    if ($arr[9] =~ /T/) {
+		if ($arr[10] =~ /T/) {
+		    $arr[9] = "both";
+		} else {
+		    $arr[9] = "in-del";
+		}
 	    } else {
-		$arr[9] = "in-del only";
-	    }
-	} else {
-	    if ($arr[10] =~ /T/) {
-		$arr[9] = "stop codon only";
-	    } else {
-		$arr[9] = "orf";
+		if ($arr[10] =~ /T/) {
+		    $arr[9] = "stop";
+		} else {
+		    $arr[9] = "orf";
+		}
 	    }
 	}
 	#status
-	if ($arr[17] =~ /T/) { $arr[11] = "unique"; }
+	if ($column == 11 && $#arr>=16 && $arr[16] =~ /T/) { $arr[11] = "unique"; }
+	#divergence
+	if ($column == 12 || $column == 16 || $column ==18) {
+	    ( $arr[$column] ) = $arr[$column] =~ /^(.+?)%?$/;
+	}
 	
 	
 	#get data
 	if ($divide =~ /\d+/) { 
 	    $arr[$column] /= $divide;
-	} elsif ($divide == "charge") {
+	} elsif ($divide eq "charge") {
 	    my $pos = () = $arr[$column] =~ /[KR]/gi;
 	    my $neg = () = $arr[$column] =~ /[DE]/gi;
 	    $arr[$column] = $pos - $neg;
@@ -239,7 +304,7 @@ sub plot {
 	}
 	
 	#put it in the right place
-	if ($type == "count") {
+	if ($type eq "count") {
 	    $categories{$arr[$column]}++;
 	} else {
 	    push @values, $arr[$column];
@@ -248,14 +313,14 @@ sub plot {
     }
     close DATA;
     
-    if ($type == "hist") {
-	
+    if ($type eq "hist") { 
+
 	#get range
 	my $min = floor(min(@values)); 
 	my $max = ceil( max(@values));
 	
 	#choose bins
-	if ($max > 700) {
+	if ($max > 1000) {
 	    #size plot, use log-scaled x
 	    $min = floor(log10($min));
 	    $max = ceil( log10($max));
@@ -295,13 +360,24 @@ sub plot {
 	}
     
     my @lines;
-    for my $cat (keys %categories) { #need to sort these
-	#line format:
-	#group x y
-	#need to determine if group is just file or file+subset (eg multiple length comparisons)
+    my $groupName = "$prj_name-$subsetType-$statType";
+    for my $cat ( sort mysort keys %categories ) {
+	#line format: group x y
 	#for status and features, group and x are reversed compared to genes
+	#if ($column == 9 || $column == 11) {
+	#    push @lines, "$cat\t$groupName\t$categories{$cat}";
+	#} else {
+	push @lines, "$groupName\t$cat\t$categories{$cat}";
+	#}
     }
     
     return \@lines;
 
+}
+
+
+sub mysort {
+    no warnings 'numeric'; #might need to add one for hash keys, too
+    my %specificOrder = ( 'orf'=>0, 'wrong_length'=>1, 'noV'=>2, 'noJ'=>3, 'noCDR3'=>4, 'indel'=>5, 'stop'=>6, 'good'=>7, 'unique'=>8, 'both'=>9 );
+    $specificOrder{$a}<=>$specificOrder{$b} || $a<=>$b || $a cmp $b;
 }
---
+++ sonar/plotting/4.2-plot_histograms.R
@@ -0,0 +1,252 @@
+#!/usr/bin/env Rscript
+
+
+usage <-function() {
+cat("
+4.2-plot_histogram.R
+
+This is a generic script for plotting nice histograms. Expected usage
+      is to be called directly by 4.1-setup_plots.pl, which parses out
+      specific desired data from annotation of the repertoire and 
+      reformats it to be compatible with this script.
+
+Usage: 4.2-plot_histogram.R data.txt outfile.png
+       			    [ title=title bars=dodge percent=T xlab=category 
+			      ylab=percent xlim=c(0,100) ylim=c(0,100)
+       			      logx=F logy=F mids=F magnify=1 showlegend=T
+			      legendpos=right h=height w=width d=dpi ]
+
+    Invoke with -h or --help to print this documentation.
+
+    data.txt      Tab-delimited file with data to be plotted. Expects 3
+    		        unlabled columns: group, x, y
+    outfile.png	  Where to save the image. Filetype will be parsed
+    		  	automatically from the extension.
+    title	  Display title of plot. Default = none.
+    bars	  Display bars for multiple groups side-by-side ('dodge')
+    		  	or stacked ('stack'). Default = dodge.
+    percent	  Boolean indicating whether the y axis should be plotted
+    		  	as percent of total for each group or raw counts.
+			Default = T.
+    xlab	  Display label for x axis. Default = 'category'.
+    ylab	  Display label for y axis. Default = 'percent' or 'number'
+    		  	depending on the value of the percent flag.
+    xlim          Display limits for x axis, specified as an R list.
+    		  	Default = chosen automatically.
+    ylim          Display limits for y axis, specified as an R list.
+    		  	Default = chosen automatically.
+    logx          Boolean indicating if the x axis should be log-scaled. If
+    		  	true, plot type is switched from bar to step.
+			Default = F.
+    logy          Boolean indicating if the y axis should be log-scaled.
+			Default = F.
+    mids          Boolean indicating if position of bars should be shifted
+    		  	so that they are centered on the midpoint of the
+			bins instead of the lower bound. Default = F.
+    magnify       Magnification factor for labels and titles. Default = 1.
+    showlegend    Boolean indicating whether legend should be displayed.
+    		  	Default = T.
+    legendpos     Position for placement of legend. Default = 'right'.
+    h             Desired height of final plot, in inches. Default = 4.
+    w		  Desired width of final plot, in inches. Default = 6.
+    dpi 	  Desired resolution of output image. Default = 150.
+
+Created by Chaim A Schramm 2015-11-19.
+
+Copyright (c) 2011-2015 Columbia University and Vaccine Research Center, National
+                               Institutes of Health, USA. All rights reserved.
+
+")
+}
+
+
+
+##########################################################################################
+#         MAIN PROGRAM
+##########################################################################################
+
+main <- function(infile, outfile, title, bars, percent, xlab, ylab, xlim, ylim, logx, logy, mids, magnify, showlegend, legendpos, h, w, d) {
+
+    data<-read.table( infile, h=F )
+
+    #make sure R plots things in the correct order:
+    data$V1 <- ordered(data$V1,levels=unique(as.character(data$V1)))
+    if (! is.numeric(data$V2)) { data$V2 <- ordered(data$V2,levels=unique(data$V2)) }
+
+    
+    #adjust X to midpoint of bin, if desired
+    if (mids) {
+       	      if (is.numeric(data$V2)) {
+	      	 	movedData <- data
+			data <- data.frame( V1=c(), V2=c(), V3=c() )
+			for ( g in levels(movedData$V1) ) {
+			      	subData <- movedData[ movedData$V1==g, ]
+			      	subData$oldX <- subData$V2
+       	      		      	for (i in seq(length(subData[,1]))) { 
+				       subData$V2[i] <- subData$oldX[i] + ( (subData$oldX[i+1]-subData$oldX[i]) / 2 )
+				}
+				data <- rbind( data, subData[1:length(subData[,1])-1, c(1,2,3)] )
+			}
+	      } else {
+			message("Option 'mids' not compatible with category plots; ignoring...")      
+	      }
+    }
+
+
+    #fill in missing points to control bar width
+    if (bars == "dodge") {
+          if (is.numeric(data$V2)) {
+       		data <- rbind( data, expand.grid(V1=levels(data$V1), V2=as.numeric(levels(as.factor(data$V2))), V3=NA) )
+		if (xlab=="category") { xlab <- "number" } #check default while we're here
+          } else {
+ 		data <- rbind( data, expand.grid(V1=levels(data$V1), V2=levels(data$V2), V3=NA) )
+          }
+    }
+
+
+    #add floor bin for step plot if x-axis is log-scaled
+    # shouldn't really matter, but add to top in reverse order so levels stay in same order when done
+    # changing data must be done before setting up plot, hence 2 if(logx) statements
+    if (logx) {
+       	      for ( g in rev(levels(data$V1)) ) {
+       	      	    newMin = 0.999 * data$V2[data$V1 == g][1]
+	      	    data <- rbind( data.frame(V1=g, V2=newMin, V3=0), data )
+	      }
+    }
+
+    # change zeroes to NAs for ylog plots
+    if (logy) {
+       	      data$V3[which(data$V3==0)] <- NA
+    }
+
+
+    if (percent) {
+       	 for (g in levels(data$V1) ) {
+		total = sum( data$V3[data$V1 == g], na.rm=T )
+		data$V3[ data$V1==g ] <- data$V3[data$V1==g] * 100 / total
+	 }
+    } else if (ylab == "percent") {
+      	      	 ylab <- "number"
+    }
+
+
+    #which column is the x axis and which is the grouping depends on use of 'stack'
+    myGroupColumn <- "V1"
+    myXposColumn  <- "V2"
+    if (bars=="stack") {
+            myGroupColumn <- "V2"
+    	    myXposColumn  <- "V1"
+    }
+
+
+    #colors generated stochastically via 
+    #http://tools.medialab.sciences-po.fr/iwanthue/
+    #and sorted for increasing hue
+    fullList = c("#BE4229", "#E74721", "#8C431C", "#CB6A27", "#E98C25", "#946E13",
+    	       	 "#D0A620", "#8B8A22", "#A9B71D", "#555C10", "#839A21", "#82B531",
+		 "#4D8426", "#55C42A", "#34992A", "#2B6B2E", "#4FC456", "#33B66D",
+		 "#4296CB", "#5A8CE2", "#3E5988", "#656CE2", "#524EA0", "#8F83CC",
+		 "#A57CE4", "#8E46AD", "#C056EB", "#CA6BE4", "#7B4D87", "#D186D7")
+    subset = fullList[floor( seq(1,length(levels(data[[myGroupColumn]]))) * 30 / length(levels(data[[myGroupColumn]])) )]
+    
+
+    #start plot
+    p <- ggplot(data, aes_string(x=myXposColumn,y="V3"))
+
+    #logx is proxy for plot type, so check first
+    if (logx) {
+       	   p <- p + geom_step(aes_string(colour=myGroupColumn), show_guide=showlegend) + 
+       	      	    scale_colour_manual( values=subset ) + scale_x_log10( )
+    } else {
+	   p <- p + geom_bar(aes_string(fill=myGroupColumn), stat='identity', position=bars, show_guide=showlegend) + 
+       	      	    scale_fill_manual( values=subset )
+    }
+        
+
+    #check y axis
+    if (logy) {
+       	      p <- p + scale_y_log10( )
+    }
+
+    myXrotation <- 0
+    myHjust     <- 0.5
+    if (nlevels(data[[myXposColumn]]) > 6) {
+            myXrotation <- 90
+	    myHjust     <- 1
+    }
+    #do all other prettifying here
+    p <- p + labs(x=xlab, y=ylab, title=title) + theme_bw() +
+      	     coord_cartesian( xlim=eval(parse(text=xlim)), ylim=eval(parse(text=ylim)) ) +
+             theme( axis.text=element_text(size=10*magnify), axis.text.x=element_text(angle=myXrotation, hjust=myHjust, vjust=0.5), 
+	     	    	axis.title=element_text(size=12*magnify),
+			legend.title=element_blank(), legend.position=legendpos, 
+			legend.text=element_text(size=10*magnify), legend.key.size=unit(0.2*magnify,"in"),
+                        legend.background=element_blank() )
+
+
+    #and output
+    ggsave(outfile, p, height=h, width=w, units="in", dpi=dpi)
+	
+}
+
+
+##########################################################################################
+#         ENTRY POINT FROM COMMAND LINE
+##########################################################################################
+
+library(ggplot2)
+library(grid)
+
+#args are:
+#  data.txt outfile.png 
+#  [ title=title bars=dodge percent=T xlab=category ylab=percent 
+#    xlim=c(0,100) ylim=c(0,100) logx=F logy=F mids=F magnify=1
+#    showlegend=T legendpos="top" h=height w=width d=dpi ]
+input = commandArgs(T)
+
+#check for parameters
+if(length(input)<2){
+  stop(usage())
+}
+
+#now check to make sure the data file is there
+if (! file.exists(input[1])) {
+  stop("Please check and make sure data file exists!\n\n")
+}
+
+
+#set defaults and process optional parameters
+infile     <- input[1]
+outfile    <- input[2]
+title      <- ""
+bars       <- "dodge"
+percent    <- T
+xlab       <- "category"
+ylab       <- "percent"
+xlim       <- NULL
+ylim       <- NULL
+logx       <- F
+logy       <- F
+mids       <- F
+magnify    <- 1
+showlegend <- T
+legendpos  <- "right"
+h          <- 4
+w          <- 6
+dpi        <- 150
+
+for (i in seq(3,length(input))) { assign(sub("=.*$", "", input[i]), sub("^[^=]*=", "", input[i])) }
+
+#check input type for numerics and booleans
+magnify    <- as.numeric(magnify)
+h          <- as.numeric(h)
+w          <- as.numeric(w)
+dpi        <- as.numeric(dpi)
+percent    <- as.logical(percent)
+logx       <- as.logical(logx)
+logy       <- as.logical(logy)
+mids       <- as.logical(mids)
+showlegend <- as.logical(showlegend)
+
+main(infile, outfile, title, bars, percent, xlab, ylab, xlim, ylim, logx, logy, mids, magnify, showlegend, legendpos, h, w, d)
+
