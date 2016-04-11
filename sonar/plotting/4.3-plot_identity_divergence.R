b51c8c77bbe0d07c6385262af3861f958c1c84cd
Chaim A Schramm <cs3037@c2b2.columbia.edu>
2016-04-11 12:09:23 -0400

    initial commit of 4.3

--- sonar/plotting/4.3-plot_identity_divergence.R
+++ sonar/plotting/4.3-plot_identity_divergence.R
@@ -1,186 +1,250 @@
 #!/usr/bin/env Rscript
 
 
-usage <-function() {
-cat("
-USAGE: longitudinal_double_sieve.r <div.list> <sieve1.list> <sieve2.list> [unique=unique.list 
-       				   mab=VRC08 gene=VH1-2 output=lds.pdf labels=dates.list 
-				   black=sieve1b.list]
+########################
+#
+# ADD CUSTOMIZATION OPTIONS:
+#     - figure size
+#     - dpi
+#     - page title
+#     - colors and contours
+#     - show/hide color guide
+#
+# GENERAL FEATURES STILL NEEDED:
+#     - implement showNames
+#     - correct default handling of axis labels
+#     - row and column titles
+#     - what should default guide behavior be?
+#
+########################
 
-The first 3 parameters are lists of other file names.
-	<div.list>	List of the divid.tab pipeline outputs for each time point, in 
-			     chronological order
-	<sieve1.list>	List of the files containing the lists of reads to plot in the 
-			     first sieve (chrono order!)
-			     **FIRST LINE IS FORMAT OF THE FILES, NOT A FILE NAME**
-			     1 = 'naked' list of reads in single column format
-			     2 = tabulated list, with column headers and ids in (for now) 
-			       	 the 2nd column (1-indexed!)
-	<seive2.list>	List of the files containing the lists of reads to plot in the 
-			     first sieve (chrono order!)
-			     **FIRST LINE IS FORMAT OF THE FILES, NOT A FILE NAME** (as above)
-			     **MULTIPLE FILES CAN BE COMBINED FOR EACH TIME POINT separated by
-			     	 a comma (no space!)**
 
-Optional parameters **MUST** be specified in the 'var=value' format.
-	<unique>	List of files containing lists of unique reads at each timepoint (to 
-			     	plot instead of using raw/bulk data).
-				Rules are same as sieve1.list above.
-	<black>		List of files containing lists of partially sieved reads at each 
-			     	timepoint to plot as black dots under the red dots in sieve2.
-				Rules are same as sieve2.list above.
-	<mab>		Identity referent to use on y-axis. Default = 1st one in the divid.tab
-				files
-	<gene>		Specify x-axis label as '% divergence from <gene>'.
-				Default = '% divergence from germline V'.
-	<output>	File name to save plots to. Default = lds.pdf.
-	<labels>	List of timepoints for labeling the longitudinal plots.
+"Usage: 4.3-plot_identity_divergence.R <idDivFile>... [ --plot <ids.list>... ] [ --sieve1 <ids.list>... --sieve2 <ids.list> ] [ --mab <abID>... ] [ --xaxis <label> --outdir </path> --output <output.pdf> --labels <dates.list> --reference <idDivFile> --showNames <0|1|2> --transpose]
 
-")
-}
+Options:
+  -h --help		   Show this documentation.
+  <idDivFile>	           Output from 2.1-calulate_id-div.pl with data for plotting.
+			       Multiple files will be treated as longitudinal time
+		    	       points and displayed in the order entered.
+  --plot <ids.list>    	   Specify a subset of sequences to be plotted (eg those from
+   	       	      	       a particular V gene). One id per line. If used, must
+			       match number and order of data files.
+  --sieve1 <ids.list>	   A subset of reads to overlay on the I-D plot as points
+   	       		       (eg those with CDR3 homology to the antibody of interest).
+			       If used, must match number and order of data files.
+  --sieve2 <ids.list>	   A second subset of reads to overlay on the I-D plot as points.
+			       If used, must match number and order of data files.
+  --mab <abID>		   Identity referent to use on y-axis. Default is the first mAb
+  			       in the first id-div.tab file. If multiple referents are
+			       specified, each will be shown in its own row of plots.
+  --xaxis <label>	   X-axis label is '% divergence from <label>'.
+  	  		       [default: germline V]
+  --outdir </path>	   Directory for saving output. [default: output/plots]
+  --output <output.pdf>	   File name of output. [default: id-div.pdf]
+  --labels <dates.list>    List of labels for the longitudinal time points, one label
+  	   		       per line. Must match number and order of data files.
+  --reference <idDivFile>  Data points to display on ALL plots, eg other members of the
+  	      		       antibody lineage of interest.
+  --showNames <0|1|2>	   Show text labels for native antibodies supplied with --reference.
+  	      		       0 = none; 1 = first plot in each row; 2 = all
+			       [default: 0]
+  -t --transpose	   Show time in rows and different antibody referents in columns.
+  			       [default: FALSE]
+			       
+
+Created by Chaim A Schramm 2013-07-11.
+Edited and commented by CAS 2015-03-09.
+Overhauled for generality CAS 2016-04-11.
+
+Copyright (c) 2013-2016 Columbia University and Vaccine Research Center, National
+                               Institutes of Health, USA. All rights reserved.
+
+" -> usage
+
 
 
 ####################################
 # PLOTTING FUNCTION
 ####################################
 
-plot_all <- function (data, native, pretty, my_x, subset=c(), plot_title=NULL, xlims=c(0,30), ylims=c(70,101), 
-	 	    	     color=TRUE, number=TRUE, xlabel=FALSE, ylabel=FALSE, contour = FALSE, conCol = 'black') {
+plot_all <- function (data, native,  heavy, plot_title=NULL, color=TRUE, guide=TRUE, xlabel=TRUE, ylabel=TRUE, contour=TRUE, conCol = 'black') {
 
-	#print(c(native, pretty, my_x, plot_title, xlims, ylims, color, number, xlabel, ylabel))
+	my_x<-paste("% divergence from", heavy)
+	my_y<-paste("% ID to", native)
 
-	my_y<-paste("% ID to", pretty)
-	if (color) my_colors=rev(rainbow(15,end=4/6))
-	else my_colors=rev(gray.colors(5))
-
-	if (length(subset) > 0)	 {
-		data<-data[which(data$read_id %in% subset),]
-	}
-	data.labels <- data.frame(x=45,y=85, label=format(length(data$germ_div),big.mark=","))
-
-	p<-ggplot(data,aes_string(x="germ_div",y=native))
-
-	if (! contour) {
-		p <- p + stat_bin2d(binwidth=c(1,1)) +
-		scale_fill_gradientn(colours=my_colors, trans="log10", limits=c(1,25000),na.value="white", guide="none")
+	if (color) {
+	   my_colors=rev(rainbow(15,end=4/6))
 	} else {
-	        library(MASS)
-		#add a smidge to default bandwidth to handle occassional datasets with super redundancy
-	        g <- kde2d(data$germ_div,data[[native]],n=101,h=1,lim=c(0,100,0,100))
-		#print(c(min(g$z[g$z>10^-10]),log10(max(g$z))))
-		densf = data.frame(expand.grid(x=g$x,y=g$y), z=as.vector(g$z))
-		r<-10^seq(-4,log10(max(g$z))+1,1.5)
-		p <- p + stat_contour(data=densf, aes(x=x,y=y,z=z), colour=conCol, size=.25, breaks=r)
+	   my_colors=rev(gray.colors(5))
 	}
 
-	p <- p + theme_bw() + labs(x=NULL, y=NULL, title=plot_title) +
-	     theme(plot.background = element_blank(),panel.grid.major = element_blank(),
+	g     <- kde2d(data$germ_div,data[[native]],n=100,h=1,lim=c(0,60,40,100))
+	densf <- data.frame(expand.grid(x=g$x, y=g$y), z=as.vector(g$z))
+	b     <- (sum(g$z) / length(data$germ_div))/2
+	t     <- b * 10^4
+	if ( max(g$z) > t ) {
+	      t <- b * 10^ceiling( log10( max(g$z)/b ) )
+	}
+	r     <- 10^seq(log10(b), log10(t), 1)
+
+	p<-ggplot(densf,aes(x,y,z=z)) +
+	 		geom_tile(aes(fill = z)) +
+			scale_fill_gradientn(colours=rev(rainbow(15,end=4/6)), trans="log10", limits=c(b,t),
+				na.value="white", breaks=r, labels=signif(r/b,1),
+			        guide = guide_colorbar( title="number of\nsequences", title.theme=element_text(size=4,angle=0),
+				        barheight = unit(.5,"in"), barwidth = unit(.1,"in"), label.theme=element_text(size=3,angle=0,) ) )+
+			theme_bw() + scale_x_continuous(expand=c(0,0),limits=c(0,50)) +
+			scale_y_continuous(expand=c(0,1),limits=c(50,100)) +
+	     		theme(plot.background = element_blank(),panel.grid.major = element_blank(),
 				axis.ticks.length = unit(.02,"in"), axis.ticks = element_line(size = .5),
-		          	panel.grid.minor = element_blank(), axis.text = element_blank(),
-				plot.margin = unit(c(.02,.02,.02,.02),"in") )
+		          	panel.grid.minor = element_blank(), axis.text = element_text(size = 6),
+				axis.title = element_text(size = 8), plot.margin = unit(c(.1,.1,.1,.1),"in"),
+				plot.title = element_text(size = 8) )
 
-	if ( xlabel ) p <- p + scale_x_continuous(expand=c(0,0),limits=xlims, my_x)
-	else p <- p + scale_x_continuous(expand=c(0,0),limits=xlims)
-
-     	if ( ylabel ) p <- p + scale_y_continuous(expand=c(0,0),limits=ylims, my_y)
-	else p <- p + scale_y_continuous(expand=c(0,0),limits=ylims)
-
-	if (number) p <- p + geom_text(data=data.labels, aes(x=x, y=y, label=label), angle=90, size=3, col="black")
+	if ( contour ) 		     { p <- p + stat_contour(colour=conCol, size=.25, breaks=10*r) }
+	if ( ! is.null(plot_title) ) { p <- p + labs( title=plot_title ) }
+	if ( xlabel ) 		     { p <- p + labs( x=my_x ) }
+	if ( ylabel ) 		     { p <- p + labs( y=my_y ) }
 
 	p
 }
+
+
+
+
+####################################
+# DISPLAY FUNCTION
+####################################
+
+# from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_%28ggplot2%29/
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
 
 
 ##########################################################################################
 #         MAIN PROGRAM
 ##########################################################################################
 
-lds <- function(file1, file2, file3, unique=NULL, nat="", gene="germline V", output="lds.pdf", labels=NULL, black=NULL) {
-
-#get the names of the data files
-div_files = scan(file1,what='character',quiet=T)
-s1 = scan(file2,what='character',quiet=T)
-s2 = scan(file3,what='character',quiet=T,blank.lines.skip=F)
-
-#process formats
-s1_type  = as.numeric(s1[1])
-s1_files = s1[-1]
-s2_type  = as.numeric(s2[1])
-s2_files = strsplit(s2[-1],",")
-
-if (length(div_files) != length(s1_files) || length(div_files) != length(s2_files)) {
-	stop("Error: number of timepoints does not match across sieve levels. Please check file formats and try again")
-}
-
-if (! is.null(unique) && file.exists(unique)) {
-	u  = scan(unique, what='character',quiet=T)
-	ut = as.numeric(u[1])
-	uf = u[-1]
-	if (length(div_files) != length(uf)) { stop ("Error: number of timepoints in unique file does not match. Please check file format and try again") }
-} else { ut = NULL }
-
-if (! is.null(black) && file.exists(black)) {
-	b  = scan(black, what='character',quiet=T,blank.lines.skip=F)
-	bt = as.numeric(b[1])
-	bf = strsplit(b[-1],",")
-	if (length(div_files) != length(bf)) { stop ("Error: number of timepoints in black file does not match. Please check file format and try again") }
-} else { bt = NULL }
-
+lds <- function(dataFileList, subsetFileList, firstSieveList, secondSieveList, natAbList, xLabel, outDir, outFile, timepointList, refPointsData, showNames, transpose) {
 
 #initiate
 myplots=list()
 
-
-#process each time point
-for (time in seq(length(div_files))) {
-
-    	#main plot
-	data <- read.table( div_files[time], h=T )
-	setU <- c()
-
-	if (! is.null(ut)) setU <- switch( ut, scan(uf[time],quiet=0), read.table(uf[time],h=T)[,2] )
-
-	if (mab == "") mab <- names(data)[3] 
-
-	myplots[[length(myplots)+1]] <- plot_all(data, mab, mab, gene, subset=setU, xlims=c(0,50), ylims=c(50,101))#, ylabel=(time==1))
-
-	#sieve1 plot
-	set1 <- switch( s1_type, scan(s1_files[time],quiet=0), read.table(s1_files[time],h=T,fill=T)[,2] )
-	ind1 <- which(data$read_id %in% set1)
-	data.labels <- data.frame(x=45,y=85, label=format(length(ind1),big.mark=","))
-	p <- plot_all(data, mab, mab, gene, subset=setU, color=FALSE, number=FALSE, contour=TRUE, xlims=c(0,50), ylims=c(50,101))#, ylabel=(time==1))
-	myplots[[length(myplots)+1]] <- p + geom_point(data=data[ind1,],colour='blue', shape=16, size=.5) + 
-				     geom_text(data=data.labels, aes(x=x, y=y, label=label), angle=90, size=3, col="blue")
-
-	#sieve2 plot
-	set2<-c()
-	for (group in s2_files[[time]]) set2 <- c(set2, switch( s2_type, scan(group,quiet=0), read.table(group,h=T,fill=T)[,2] ))
-	ind2 <- which(data$read_id %in% set2)
-	data.labels <- data.frame(x=45,y=90, label=format(length(ind2),big.mark=","))
-	p <- plot_all(data, mab, mab, gene, subset=set1, color=FALSE, number=FALSE, contour=TRUE, conCol='cornflowerblue', xlims=c(0,50), ylims=c(50,101))#, ylabel=(time==1), xlabel=TRUE)
-
-	if (! is.null(bt)) {
-	      setB <- c()
-	      for (group in bf[[time]]) setB <- c(setB, switch( bt, scan(group,quiet=0), read.table(group,h=T,fill=T)[,2] ))
-	      indB <- which(data$read_id %in% setB)
-	      data.Blabels <- data.frame(x=45,y=70, label=format(length(indB),big.mark=","))
-	      p <- p + geom_point(data=data[indB,],colour='goldenrod', shape=16, size=1) +
-	      	       		geom_text(data=data.Blabels, aes(x=x, y=y, label=label), angle=90, size=3, col="goldenrod")
-	}
-
-	myplots[[length(myplots)+1]] <- p + geom_point(data=data[ind2,],aes_string(x="germ_div", y=mab),colour='darkorchid4', shape=16, size=1) + 
-				     geom_text(data=data.labels, aes(x=x, y=y, label=label), angle=90, size=3, col="darkorchid4")
-
+if ( ! is.null(refPointsData) ) {
+   refData <- read.table( refPointsData, h=T )
 }
 
-	row_titles <- c("All", "Sieve1", "Sieve2")
-	if (! is.null(labels)) col_titles = scan(labels,quiet=T)
+#process each time point
+for ( time in seq(length(dataFileList)) ) {
 
-	#and print to file	
-	pdf(output, height=3, width=length(div_files))
-	multiplot(plotlist=myplots,rows=3,layout=matrix(seq(2, 1+3*length(div_files)),nrow=3, byrow=F))#, row_titles=row_titles)
-	dev.off()
-#break
+    	#main plot
+	bigdata   <- read.table( dataFileList[time], h=T )
+	smalldata <- bigdata
+
+	if ( is.null(natAbList) )  natAbList <- c( names(bigdata)[3] )
+
+	for (mab in natAbList) {
+
+	    if ( ! is.null(subsetFileList) ) {
+	       subset    <- scan(subsetFileList[time], what='character', quiet=T)
+	       smalldata <- bigdata[which(bigdata$ID %in% subset),]
+	    }
+
+	    ####
+	    # ADD OPTIONS (plot color, contours, yes/no to axis labels, scale bar)
+	    ####
+	    p <- plot_all(smalldata, mab, xLabel)
+
+	    if ( ! is.null(firstSieveList) ) {
+	       subset <- scan(firstSieveList[time], what='character', quiet=T)
+	       s1data <- bigdata[which(bigdata$ID %in% subset),]
+	       p <- p + geom_point(data=s1data,aes_string(x="germ_div",y=mab,z=NA),colour='goldenrod', shape=16, size=.5)
+	    }
+
+	    if ( ! is.null(secondSieveList) ) {
+	       subset <- scan(secondSieveList[time], what='character', quiet=T)
+	       s2data <- bigdata[which(bigdata$ID %in% subset),]
+	       p <- p + geom_point(data=s2data,aes_string(x="germ_div",y=mab,z=NA),colour='magenta', shape=16, size=.5)
+	    }
+
+	    if ( ! is.null(refPointsData) ) {
+	       #####
+	       # NEED TO HANDLE SHOW_NAMES OPTION HERE
+	       #####
+	       p <- p + geom_point(data=refData,aes_string(x="germ_div",y=mab,z=NA),colour='black', shape=4, size=1)
+	    }
+
+	    myplots[[length(myplots)+1]] <- p
+
+	} #next mab in list
+
+} #next time point
+
+
+numCols <- length(dataFileList)
+numRows <- length(natAbList)
+
+if ( transpose ) {
+   numCols <- length(natAbList)
+   numRows <- length(dataFileList)
+}
+
+height  <- 2*numRows
+width   <- 2.5*numCols
+
+ggsave(
+	paste(outDir,outFile,sep="/"),
+	multiplot( plotlist=myplots, cols=numCols ),
+	dpi = 600,
+	height = height,
+	width = width
+)
+
 
 }
 
@@ -190,36 +254,42 @@ for (time in seq(length(div_files))) {
 #         ENTRY POINT FROM COMMAND LINE
 ##########################################################################################
 
+library(docopt)
 library(ggplot2)
 library(grid)
 library(MASS)
-source('/ifs/home/c2b2/ls_nih/cs3037/scripts/multiplot.R')
 
-# args are: <div.list> <sieve1.list> <sieve2.list> [unique=unique.list mab=VRC08 gene=VH1-2 output=lds.pdf labels=dates.list black=sieve1b.list]
-input = commandArgs(T)
 
-#check for parameters
-if(length(input)<3){
-  stop(usage())
-} else if (length(input)>8) {
-  warning("I only know what to do with the first 8 parameters; ignoring the rest.....\n\n")
+opts<-docopt(usage, strip=T, help=T)
+
+
+# check to make sure the files are there
+if (! all(file.exists(opts$idDivFile))) {
+  stop("Cannot find 1 or more input files...\n\n")
 }
 
-#now check to make sure the files are there
-if (! all(file.exists(input[1:3]))) {
-  stop("Please check input parameters and make sure all files exist!\n\n")
+if (! is.null(opts$plot)) {
+   if ( length(opts$plot) != length(opts$idDivFile) ) { stop("Please make sure that the number of plot files matches the number of input files and that they are in the same order\n\n") }
+   if (! all(file.exists(opts$plot))) {
+      stop("Cannot find 1 or more plot files...\n\n")
+   }
 }
 
+if (! is.null(opts$sieve1)) {
+   if ( length(opts$sieve1) != length(opts$idDivFile) ) { stop("Please make sure that the number of sieve1 files matches the number of input files and that they are in the same order\n\n") }
+   if (! all(file.exists(opts$sieve1))) {
+      stop("Cannot find 1 or more sieve1 files...\n\n")
+   }
+}
 
-#set defaults and process optional parameters
-unique<-NULL
-mab<-""
-gene<-"germline V"
-output<-"lds.pdf"
-labels<-NULL
-black<-NULL
+if (! is.null(opts$sieve2)) {
+   if ( length(opts$sieve2) != length(opts$idDivFile) ) { stop("Please make sure that the number of sieve2 files matches the number of input files and that they are in the same order\n\n") }
+   if (! all(file.exists(opts$sieve2))) {
+      stop("Cannot find 1 or more sieve2 files...\n\n")
+   }
+}
 
-for (i in seq(4,length(input))) { assign(sub("=.*$", "", input[i]), sub("^[^=]*=", "", input[i])) }
+if (! is.null(opts$reference) && ! file.exists(opts$reference)) { stop("Cannot find reference data file...\n\n") }
+ 
 
-lds(input[1], input[2], input[3], unique, mab, gene, output, labels, black)
-
+lds(opts$idDivFile, opts$plot, opts$sieve1, opts$sieve2, opts$mab, opts$xaxis, opts$outdir, opts$output, opts$labels, opts$reference, opts$showNames, opts$transpose)
