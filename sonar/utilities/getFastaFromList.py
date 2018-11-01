a7bda976908e98f9a9da6cbdd19f18ae2e15c6a2
Chaim A Schramm <chaim.schramm@nih.gov>
2018-11-01 14:44:57 -0400

    added fastq and gzip compatibility

--- sonar/utilities/getFastaFromList.py
+++ sonar/utilities/getFastaFromList.py
@@ -17,14 +17,16 @@ Created by Chaim A Schramm on 2015-04-27.
 Added streaming from STDIN on 2016-06-10.
 Added streaming to STDOUT on 2017-03-19.
 Edited to use Py3 and DocOpt by CAS 2018-08-28.
+Added FastQ and GZip support by CAS 2018-11-01.
 
 Copyright (c) 2011-2018 Columbia University and Vaccine Research Center, National
                          Institutes of Health, USA. All rights reserved.
 
 """
 
-import sys, fileinput
+import sys, fileinput, gzip
 from docopt import docopt
+from functools import partial
 try:
 	from sonar import *
 except ImportError:
@@ -35,8 +37,20 @@ except ImportError:
 
 def loadAndAnnotate(seqFile, saveDict):
 	good = 0
-	with open(seqFile, "rU") as seqs:
-		for s in SeqIO.parse(seqs, "fasta"):
+
+	#fasta or fastq
+	read_format="fasta"
+	if re.search("\.(fq|fastq)", seqFile) is not None:
+		read_format = "fastq"
+
+	#gzip?
+	if re.search("gz$", seqFile):
+		_open = partial(gzip.open,mode='rt')
+	else:
+		_open=open
+		
+	with _open(seqFile) as seqs:
+		for s in SeqIO.parse(seqs, read_format):
 			if s.id not in saveDict:
 				continue
 			else:
