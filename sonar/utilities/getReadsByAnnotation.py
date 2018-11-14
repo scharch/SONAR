8cf595a89ad5d275c69e8387d356d3b08847f94b
Chaim A Schramm <chaim.schramm@nih.gov>
2018-11-14 11:51:58 -0500

    added some more options for extracting read subsets

--- sonar/utilities/getReadsByAnnotation.py
+++ sonar/utilities/getReadsByAnnotation.py
@@ -7,24 +7,28 @@ This is a utility script to find all sequences in a large fasta that have
       an annotation of interest (eg a particular V gene assignment or
       CDR3 motif).
 
-Usage: getReadsByAnnotations.py -f file.fa -a annotation [ -o output.fa ]
+Usage: getReadsByAnnotations.py -f file.fa ( -a annotation | -l list ) [ -o output.fa -m 0 ]
 
 Options:
     -f file.fa      Fasta file containing the sequences to be subsetted.
     -a annotation   Regular expression to look for in the fasta def line
                        of each sequence.
+    -l list         A list of annotations to look for. STDIN or a file name.
     -o output.fa    Fasta file in which to save extracted sequences. 
                        [default: STDOUT]
+    -m 0            Max number of matches to find (0 for all matches).
+                       [default: 0]
 
 
 Created by Chaim A Schramm on 2018-11-01.
+Added max matches and list options by CAS 2018-11-13.
 
 Copyright (c) 2011-2018 Vaccine Research Center, National Institutes of
                          Health, USA. All rights reserved.
 
 """
 
-import sys, re
+import sys, re, fileinput
 from docopt import docopt
 try:
 	from sonar import *
@@ -34,16 +38,25 @@ except ImportError:
 	from sonar import *
 
 
-def checkAnnotation(seqFile):
+def annotationInList(annotation,refList):
+	for a in refList:
+		if re.search( a, annotation ):
+			return True
+	return False
+
+	
+def checkAnnotation(seqFile, annotationList):
 	good = 0
-        read_format="fasta"
-        if re.search("\.(fq|fastq)$", seqFile) is not None:
-                read_format="fastq"
+	read_format="fasta"
+	if re.search("\.(fq|fastq)$", seqFile) is not None:
+		read_format="fastq"
 	with open(seqFile, "rU") as seqs:
 		for s in SeqIO.parse(seqs, read_format):
-			if re.search( arguments['-a'], s.description ):
+			if annotationInList( s.description, annotationList ):
 				good += 1
 				yield s
+				if good == arguments['-m']:
+					break
 				if good % 100000 == 0:
 					sys.stderr.write("Loaded %d so far...\n" % good)
 
@@ -51,9 +64,16 @@ def checkAnnotation(seqFile):
 
 def main():
 
-    if arguments['-o'] != "STDOUT":
-	    sys.stdout = open(arguments['-o'], "w")
-    SeqIO.write(checkAnnotation(arguments['-f']), sys.stdout, "fasta")
+	annotationList = []
+	if arguments['-l'] is not None:
+		for line in fileinput.input(arguments['-l']):
+			annotationList.append(line.strip())
+	else:
+		annotationList.append(arguments['-a'])
+		
+	if arguments['-o'] != "STDOUT":
+		sys.stdout = open(arguments['-o'], "w")
+	SeqIO.write(checkAnnotation(arguments['-f'], annotationList), sys.stdout, "fasta")
 
 
 
@@ -61,7 +81,11 @@ def main():
 if __name__ == '__main__':
 
 	arguments = docopt(__doc__)
+	arguments['-m'] = int(arguments['-m'])
 
+	if arguments['-l'] == "STDIN":
+		arguments['-l'] = "-"
+	
 	#log command line
 	logCmdLine(sys.argv)
 
