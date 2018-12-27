e324ea5ef6801b4b4ad56662c66fb0c9b87299b8
Chaim A Schramm <chaim.schramm@nih.gov>
2018-12-27 17:20:58 -0500

    folded removeListFromFasta.py function into the getFastaFromList.py utility using the -r flag

--- utilities/getFastaFromList.py
+++ utilities/getFastaFromList.py
@@ -6,18 +6,20 @@ getFastaFromList.py
 This is a simple utility script for efficiently extracting a subset of
       sequences from a large fasta file.
 
-Usage: getFastaFromList.py -f <seqs.fa> [ -o <output.fa> -l <list.txt> ]
+Usage: getFastaFromList.py -f <seqs.fa> [ -o <output.fa> -l <list.txt> -r ]
 
 Options:
     -f <seqs.fa>     Fasta file containing the sequences to be subsetted.
     -o <output.fa>   Fasta file in which to save extracted sequences. [default: STDOUT]
     -l <list.txt>    Text file containing list of sequence identifiers to extract. [default: STDIN]
+    -r               Remove sequences in the list and output all others. [default: False]
 
 Created by Chaim A Schramm on 2015-04-27.
 Added streaming from STDIN on 2016-06-10.
 Added streaming to STDOUT on 2017-03-19.
 Edited to use Py3 and DocOpt by CAS 2018-08-28.
 Added FastQ and GZip support by CAS 2018-11-01.
+Added -r flag by CAS 2018-12-27.
 
 Copyright (c) 2011-2018 Columbia University and Vaccine Research Center, National
                          Institutes of Health, USA. All rights reserved.
@@ -51,9 +53,7 @@ def loadAndAnnotate(seqFile, saveDict):
 		
 	with _open(seqFile) as seqs:
 		for s in SeqIO.parse(seqs, read_format):
-			if s.id not in saveDict:
-				continue
-			else:
+			if (s.id in saveDict) != (arguments['-r']): #exclusive or
 				good += 1
 				s.description = s.description + " " + saveDict.get(s.id, "")
 				yield s
