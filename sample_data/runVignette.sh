3b132b6863bc59dd1bcb36bec8e9af03a8ba6db9
Chaim Schramm <schrammca@ai-hpcn027.niaid.nih.gov>
2019-07-10 13:41:58 -0400

    Adjusted vignette script to work without X11, if necessary

--- sample_data/runVignette.sh
+++ sample_data/runVignette.sh
@@ -11,7 +11,9 @@ cat << EOF
 * This script will run the vignette using the CAP256 data from Doria-Rose et al Nature 2014. *
 * It assumes that you have already verified that all dependencies are properly installed and *
 * have run setup.py. If running in the Docker container, please make sure to start Docker    *
-* '-e DISPLAY=\$DISPLAY' so that it can open X11 windows.                                    *
+* '-e DISPLAY=\$DISPLAY' so that it can open X11 windows. Otherwise, the script will skip     *
+* 2.2-get_island_interactive.R and instead load the preselected islands from the sample_data *
+* folder.                                                                                    *
 * This script will use 4 threads and should take approximately 15-20 hours to complete on an *
 * average machine. Please note that it will wait for user input (to select the island(s)     *
 * that correspond to lineage-related reads in 2.2) for each of the three time points; this   *
@@ -35,10 +37,15 @@ sonar cluster_Sequences --id .97 --min2 2
 
 echo "Running Module 2"
 sonar id-div -a /SONAR/sample_data/CAP256-VRC26.01-12H.fa -t 4
-sonar get_island output/tables/cap256-week34H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
+
+if [[ -n "$DISPLAY" ]]; then
+	sonar get_island output/tables/cap256-week34H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
+	sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week34H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week34H_islandSeqs.fa
+else
+	cp /SONAR/sample_data/cap256-week34H_islandSeqs.fa output/sequences/nucleotide
+fi
 sonar intradonor --n /SONAR/sample_data/CAP256-VRC26.01-12H.fa --v IGHV3-30*18 --threads 4 -f
 sonar groups -v 'IGHV3-30*18' -j 'IGHJ3*01' -t 8
-sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week34H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week34H_islandSeqs.fa
 
 echo "Processing week 48 data"
 cd ..
@@ -50,8 +57,12 @@ sonar blast_J --threads 4
 sonar finalize
 sonar cluster_Sequences --id .97 --min2 2
 sonar id-div -a /SONAR/sample_data/CAP256-VRC26.01-12H.fa -t 4
-sonar get_island output/tables/cap256-week48H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
-sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week48H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week48H_islandSeqs.fa
+if [[ -n "$DISPLAY" ]]; then
+	sonar get_island output/tables/cap256-week48H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
+	sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week48H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week48H_islandSeqs.fa
+else
+	cp /SONAR/sample_data/cap256-week48H_islandSeqs.fa output/sequences/nucleotide
+fi
 
 echo "Processing week 59 data"
 cd ..
@@ -63,8 +74,12 @@ sonar blast_J --threads 4
 sonar finalize
 sonar cluster_Sequences --id .97 --min2 2
 sonar id-div -a /SONAR/sample_data/CAP256-VRC26.01-12H.fa -t 4
-sonar get_island output/tables/cap256-week59H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
-sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week59H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week59H_islandSeqs.fa
+if [[ -n "$DISPLAY" ]]; then
+	sonar get_island output/tables/cap256-week59H_goodVJ_unique_id-div.tab --mab CAP256-VRC26.01 --mab CAP256-VRC26.08
+	sonar getfasta -l output/tables/islandSeqs.txt  -f output/sequences/nucleotide/cap256-week59H_goodVJ_unique.fa -o output/sequences/nucleotide/cap256-week59H_islandSeqs.fa
+else
+	cp /SONAR/sample_data/cap256-week59H_islandSeqs.fa output/sequences/nucleotide
+fi
 
 echo "Starting longitudinal analysis"
 cd ..
@@ -73,4 +88,4 @@ cd cap256-longitudinal
 sonar merge_time --seqs ../cap256-week34H/output/sequences/nucleotide/cap256-week34H_islandSeqs.fa --labels w34 --seqs ../cap256-week48H/output/sequences/nucleotide/cap256-week48H_islandSeqs.fa --labels w48 --seqs ../cap256-week59H/output/sequences/nucleotide/cap256-week59H_islandSeqs.fa --labels w59
 sonar igphyml -v 'IGHV3-30*18' --quick -f
 sonar fliptree output/cap256-longitudinal_igphyml.tree output/cap256-longitudinal_igphyml.flipped.tree
-sonar display_tree -t output/cap256-longitudinal_igphyml.flipped.tree -n /SONAR/sample_data/natives.csv -o output/plot/CAP256_SONAR-vignette_tree.png --sp 2 --sc 1500 -f 2
+xvfb-run sonar display_tree -t output/cap256-longitudinal_igphyml.flipped.tree -n /SONAR/sample_data/natives.csv -o output/plot/CAP256_SONAR-vignette_tree.png --sp 2 --sc 1500 -f 2
