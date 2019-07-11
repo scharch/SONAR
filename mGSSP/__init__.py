cb3128ad0a1cda7aba598a67b5f381c63990ab06
Chaim Schramm <schrammca@ai-hpcn098.niaid.nih.gov>
2019-07-11 15:37:56 -0400

    Moved Vgene alignment dict to customizable csv file and added Sheng et al 2017 GSSPs to sample_data folder

--- mGSSP/__init__.py
+++ mGSSP/__init__.py
@@ -44,7 +44,13 @@ class GSSP:
 
 
 	#this function compares the GSSPs from all V genes between two sets of profiles
-	def compare(self, otherSpectrum, subset=""):
+	def compare(self, otherSpectrum, alignment="<SONAR>/sample_data/GSSPs/comparison_dictionary.csv", subset=""):
+		alignment = re.sub( "<SONAR>", SCRIPT_FOLDER, alignment)
+		alignDict = dict()
+		with open(alignment, 'r') as handle:
+			alignCSV = csv.reader(handle)
+			for row in alignCSV:
+				alignDict[ row[0] ] = dict( ins=[int(x) for x in row[1].split(",") if not row[1]==""], dels=[int(x) for x in row[2].split(",") if not row[2]==""] )
 		results = defaultdict( dict )
 		for v1 in self.vgenes:
 			if subset not in v1:
@@ -55,14 +61,20 @@ class GSSP:
 				comps = []
 				for i in self.vgenes[v1]:
 					for j in otherSpectrum.vgenes[v2]:
-						comps.append( spectrumJSD(i,j,v1,v2) )
+						comps.append( spectrumJSD(i,j,alignDict,v1,v2) )
 				results["%s,%s"%(self.name,v1)]["%s,%s"%(otherSpectrum.name,v2)] = average(comps)
 
 		return pandas.DataFrame(results)
 		
 	
 	#this function compares GSSPs between V genes in the current set of profiles
-	def betweenV(self, subset=""):
+	def betweenV(self, alignment="<SONAR>/sample_data/GSSPs/comparison_dictionary.csv", subset=""):
+		alignment = re.sub( "<SONAR>", SCRIPT_FOLDER, alignment)
+		alignDict = dict()
+		with open(alignment, 'r') as handle:
+			alignCSV = csv.reader(handle)
+			for row in alignCSV:
+				alignDict[ row[0] ] = dict( ins=[int(x) for x in row[1].split(",") if not row[1]==""], dels=[int(x) for x in row[2].split(",") if not row[2]==""] )
 		results = defaultdict( dict )
 		k = sorted( self.vgenes.keys() )
 		for n, v1 in enumerate(k):
@@ -75,7 +87,7 @@ class GSSP:
 				comps = []
 				for i in self.vgenes[v1]:
 					for j in self.vgenes[v2]:
-						comps.append( spectrumJSD(i,j,v1,v2) )
+						comps.append( spectrumJSD(i,j,alignDict,v1,v2) )
 				results["%s,%s"%(self.name,v1)]["%s,%s"%(self.name,v2)] = average(comps)
 				results["%s,%s"%(self.name,v2)]["%s,%s"%(self.name,v1)] = average(comps)
 
@@ -185,127 +197,10 @@ def letter2profile( letter ):
 
 
 
-def spectrumJSD( spectrum1, spectrum2, v1=None, v2=None ):
+def spectrumJSD( spectrum1, spectrum2, indels, v1=None, v2=None ):
 
 	if not v1 == v2:
 		#note - this dictionary is in position (1-based) counting...
-		indels = { 'IGHV1-18'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-2'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-24'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-3'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-45'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-46'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-58'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-69'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-8'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-C'	:  dict( ins=[], dels=[] ),
-			   'IGHV1-F'	:  dict( ins=[], dels=[] ),
-			   'IGHV2-26'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV2-5'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV2-70'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV3-11'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-13'	:  dict( ins=[], dels=[54] ),
-			   'IGHV3-15'	:  dict( ins=[54,55], dels=[] ),
-			   'IGHV3-16'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-20'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-21'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-23'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-30'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-30-3' :  dict( ins=[], dels=[] ),
-			   'IGHV3-33'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-35'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-38'	:  dict( ins=[], dels=[53,54] ),
-			   'IGHV3-43'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-48'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-49'	:  dict( ins=[54,55], dels=[] ),
-			   'IGHV3-53'	:  dict( ins=[], dels=[54] ),
-			   'IGHV3-64'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-66'	:  dict( ins=[], dels=[54] ),
-			   'IGHV3-7'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-72'	:  dict( ins=[59,60,61], dels=[54] ),
-			   'IGHV3-73'	:  dict( ins=[59,60,61], dels=[54] ),
-			   'IGHV3-74'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-9'	:  dict( ins=[], dels=[] ),
-			   'IGHV3-D'	:  dict( ins=[], dels=[53,54] ),
-			   'IGHV3-NL1'	:  dict( ins=[], dels=[] ),
-			   'IGHV4-28'	:  dict( ins=[31], dels=[54] ),
-			   'IGHV4-30-2' :  dict( ins=[31,32], dels=[54] ),
-			   'IGHV4-30-4' :  dict( ins=[31,32], dels=[54] ),
-			   'IGHV4-31'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV4-34'	:  dict( ins=[], dels=[54] ),
-			   'IGHV4-39'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV4-4'	:  dict( ins=[31], dels=[54] ),
-			   'IGHV4-59'	:  dict( ins=[], dels=[54] ),
-			   'IGHV4-61'	:  dict( ins=[31,32], dels=[54] ),
-			   'IGHV4-B'	:  dict( ins=[31], dels=[54] ),
-			   'IGHV5-51'	:  dict( ins=[], dels=[] ),
-			   'IGHV5-A'	:  dict( ins=[], dels=[] ),
-			   'IGHV6-1'	:  dict( ins=[31,32,61,62], dels=[54] ),
-			   'IGHV7-4-1'	:  dict( ins=[], dels=[] ),
-			   'IGHV7-81'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-12'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-16'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-17'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-27'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-33'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-37'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-39'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-5'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-6'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-8'	:  dict( ins=[], dels=[] ),
-			   'IGKV1-9'	:  dict( ins=[], dels=[] ),
-			   'IGKV2-24'	:  dict( ins=[30,31,32,33,34], dels=[] ),
-			   'IGKV2-28'	:  dict( ins=[30,31,32,33,34], dels=[] ),
-			   'IGKV2-30'	:  dict( ins=[30,31,32,33,34], dels=[] ),
-			   'IGKV2-40'	:  dict( ins=[30,31,32,33,34,35], dels=[] ),
-			   'IGKV3-11'	:  dict( ins=[], dels=[] ),
-			   'IGKV3-15'	:  dict( ins=[], dels=[] ),
-			   'IGKV3-20'	:  dict( ins=[30], dels=[] ),
-			   'IGKV3-7'	:  dict( ins=[30], dels=[] ),
-			   'IGKV4-1'	:  dict( ins=[30,31,32,33,34,35], dels=[] ),
-			   'IGKV5-2'	:  dict( ins=[], dels=[] ),
-			   'IGKV6-21'	:  dict( ins=[], dels=[] ),
-			   'IGKV6-41'	:  dict( ins=[], dels=[] ),
-			   'IGLV1-36'	:  dict( ins=[], dels=[] ),
-			   'IGLV1-40'	:  dict( ins=[33], dels=[] ),
-			   'IGLV1-41'	:  dict( ins=[], dels=[] ),
-			   'IGLV1-44'	:  dict( ins=[], dels=[] ),
-			   'IGLV1-47'	:  dict( ins=[], dels=[] ),
-			   'IGLV1-50'	:  dict( ins=[33], dels=[] ),
-			   'IGLV1-51'	:  dict( ins=[], dels=[] ),
-			   'IGLV10-54'	:  dict( ins=[], dels=[] ),
-			   'IGLV11-55'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV2-11'	:  dict( ins=[33], dels=[] ),
-			   'IGLV2-14'	:  dict( ins=[33], dels=[] ),
-			   'IGLV2-18'	:  dict( ins=[33], dels=[] ),
-			   'IGLV2-23'	:  dict( ins=[33], dels=[] ),
-			   'IGLV2-33'	:  dict( ins=[33], dels=[] ),
-			   'IGLV2-8'	:  dict( ins=[33], dels=[] ),
-			   'IGLV3-1'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-10'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-12'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-16'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-19'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-21'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-22'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-25'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-27'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-32'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV3-9'	:  dict( ins=[], dels=[27,28] ),
-			   'IGLV4-3'	:  dict( ins=[31,54,55,56,57], dels=[31,32] ),
-			   'IGLV4-60'	:  dict( ins=[31,54,55,56,57], dels=[31,32] ),
-			   'IGLV4-69'	:  dict( ins=[31,54,55,56,57], dels=[31,32] ),
-			   'IGLV5-37'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV5-39'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV5-45'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV5-48'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV5-52'	:  dict( ins=[33,56,57,58,59,73,74], dels=[] ),
-			   'IGLV6-57'	:  dict( ins=[68,69], dels=[] ),
-			   'IGLV7-43'	:  dict( ins=[33], dels=[] ),
-			   'IGLV7-46'	:  dict( ins=[33], dels=[] ),
-			   'IGLV8-61'	:  dict( ins=[33], dels=[] ),
-			   'IGLV9-49'	:  dict( ins=[31], dels=[31,32] )
-			       }
 		
 		try:
 			#kludgy but: use set notation to get differences (so if comparing 4-4 to 4-28, then no need to change anything)
